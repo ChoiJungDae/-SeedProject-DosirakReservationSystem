dbf66b0bb59b4133964dadf284d09dcafaaf1063
ChoiJungdae <chojundae87@gmail.com>
2016-12-15 14:16:09 +0900

    hello폴더 추가, index파일 변경

--- dosirak_v1/src/main/java/seed/edu/dosirak/controller/DosirakController.java
+++ dosirak_v1/src/main/java/seed/edu/dosirak/controller/DosirakController.java
@@ -26,11 +26,17 @@ public class DosirakController {
  @Resource(name="DosirakService")
  private DosirakService memberDAO;
  
- @RequestMapping(value="/index")
+ @RequestMapping(value="/index.do")
 	 public ModelAndView index(){
 		 ModelAndView mv  = new ModelAndView("index");
 		 return mv;
 	 }
+ 
+ @RequestMapping(value="/hello.do")
+ public ModelAndView hello(){
+	 ModelAndView mv  = new ModelAndView("jsp/hello");
+	 return mv;
+ }
  
 /* @RequestMapping(value = "/member/create.do", method = RequestMethod.GET)
  public ModelAndView create() {
@@ -51,38 +57,38 @@ public class DosirakController {
  
    
    
-// ����, ���� �߰� ---------------------------------------------------------    
+// 占쏙옙占쏙옙, 占쏙옙占쏙옙 占쌩곤옙 ---------------------------------------------------------    
     memberVO.setAuth(Tool.key()); // ABC012345678901234
     memberVO.setDropout("N");
      
-    if (memberDAO.admin_search("M") == 0){ // �÷���, ������ ����
-      msgs.add("���� ��� ���������� Master �����Դϴ�.<br><br>");
-      memberVO.setAct("M");  // �ְ� ������ ����
-      memberVO.setConfirm("Y"); //  �ְ� ������������ ���� Ȯ�� ó��
+    if (memberDAO.admin_search("M") == 0){ // 占시뤄옙占쏙옙, 占쏙옙占쏙옙占쏙옙 占쏙옙占쏙옙
+      msgs.add("占쏙옙占쏙옙 占쏙옙占� 占쏙옙占쏙옙占쏙옙占쏙옙占쏙옙 Master 占쏙옙占쏙옙占쌉니댐옙.<br><br>");
+      memberVO.setAct("M");  // 占쌍곤옙 占쏙옙占쏙옙占쏙옙 占쏙옙占쏙옙
+      memberVO.setConfirm("Y"); //  占쌍곤옙 占쏙옙占쏙옙占쏙옙占쏙옙占쏙옙占쏙옙 占쏙옙占쏙옙 확占쏙옙 처占쏙옙
     }else{
-      memberVO.setAct("H");  // �����ڰ� ������ �ʿ���, H: hold.
-      memberVO.setConfirm("N"); // ���� Ȯ�� �ȵ�, �����ڰ� ���� Ȯ�� ����. 
+      memberVO.setAct("H");  // 占쏙옙占쏙옙占쌘곤옙 占쏙옙占쏙옙占쏙옙 占십울옙占쏙옙, H: hold.
+      memberVO.setConfirm("N"); // 占쏙옙占쏙옙 확占쏙옙 占싫듸옙, 占쏙옙占쏙옙占쌘곤옙 占쏙옙占쏙옙 확占쏙옙 占쏙옙占쏙옙. 
     }
     // ---------------------------------------------------------     
- // ��й�ȣ ��ȣȭ
+ // 占쏙옙橘占싫� 占쏙옙호화
  // ---------------------------------------------------------     
-     System.out.println("��й�ȣ ��ȣȭ ��: "+memberVO.getM_id());
+     System.out.println("占쏙옙橘占싫� 占쏙옙호화 占쏙옙: "+memberVO.getM_id());
      
      AES256Util aes256 = new AES256Util();
      String encrypt_pwd = aes256.aesEncode(memberVO.getM_id());
      memberVO.setM_id(new String(encrypt_pwd));
-     System.out.println("��й�ȣ ��ȣȭ ��"+memberVO.getM_id());
+     System.out.println("占쏙옙橘占싫� 占쏙옙호화 占쏙옙"+memberVO.getM_id());
  // ---------------------------------------------------------     
      if (memberDAO.create(memberVO) == 1) {
-       msgs.add("ȸ�������� ó�� �Ǿ����ϴ�.");
-       msgs.add("�������ּż� �����մϴ�.");
-       msgs.add("�̸��� ���� �� �α����� �����մϴ�.");
-       links.add("<button type='button' onclick=\"location.href='../index.jsp'\">Ȩ������</button>");
+       msgs.add("회占쏙옙占쏙옙占쏙옙占쏙옙 처占쏙옙 占실억옙占쏙옙占싹댐옙.");
+       msgs.add("占쏙옙占쏙옙占쏙옙占쌍셔쇽옙 占쏙옙占쏙옙占쌌니댐옙.");
+       msgs.add("占싱몌옙占쏙옙 占쏙옙占쏙옙 占쏙옙 占싸깍옙占쏙옙占쏙옙 占쏙옙占쏙옙占쌌니댐옙.");
+       links.add("<button type='button' onclick=\"location.href='../index.jsp'\">홈占쏙옙占쏙옙占쏙옙</button>");
      } else {
-       msgs.add("ȸ�� ���Կ� �����߽��ϴ�.");
-       msgs.add("�˼������� �ٽ��ѹ� �õ����ּ���.");
-       links.add("<button type='button' onclick=\"history.back()\">�ٽýõ�</button>");
-       links.add("<button type='button' onclick=\"location.href='../index.jsp'\">Ȩ������</button>");
+       msgs.add("회占쏙옙 占쏙옙占쌉울옙 占쏙옙占쏙옙占쌩쏙옙占싹댐옙.");
+       msgs.add("占싯쇽옙占쏙옙占쏙옙占쏙옙 占쌕쏙옙占싼뱄옙 占시듸옙占쏙옙占쌍쇽옙占쏙옙.");
+       links.add("<button type='button' onclick=\"history.back()\">占쌕시시듸옙</button>");
+       links.add("<button type='button' onclick=\"location.href='../index.jsp'\">홈占쏙옙占쏙옙占쏙옙</button>");
      }
   
      mav.addObject("msgs", msgs);
